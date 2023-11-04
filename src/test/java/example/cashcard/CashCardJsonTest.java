2e04b12e162ee81ddcc2039a63182653d31feceb
Joel Amoako <joelkofiamoako@gmail.com>
2023-11-04 16:23:18 +0000

    Quick Auto Commit

---
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,12 @@
+package example.cashcard;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CashCardJsonTest {
+
+    @Test
+    public void myFirstTest() {
+        assertThat(42).isEqualTo(42);
+    }
+}
